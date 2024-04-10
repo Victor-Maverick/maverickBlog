8d3f842681bbccc8f5d819959732e23d2165335a
Victor-Maverick <victormsonter@gmail.com>
2024-04-10 16:39:43 +0100

    commit

--- src/main/java/africa/semicolon/maverickblog/data/model/User.java
+++ src/main/java/africa/semicolon/maverickblog/data/model/User.java
@@ -1,14 +1,18 @@
 package africa.semicolon.maverickblog.data.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import javax.annotation.processing.Generated;
 
 @Entity
+@RequiredArgsConstructor
+@Data
 @Table(name = "user_table")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String username;
     private String password;
--- src/main/java/africa/semicolon/maverickblog/data/model/View.java
+++ src/main/java/africa/semicolon/maverickblog/data/model/View.java
@@ -1,15 +1,18 @@
 package africa.semicolon.maverickblog.data.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Data
 @Table(name="view_table")
 public class View {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
+    @ManyToOne
     private User viewer;
-    private LocalDateTime dateViewed;
+    private LocalDateTime dateViewed = LocalDateTime.now();
 }
