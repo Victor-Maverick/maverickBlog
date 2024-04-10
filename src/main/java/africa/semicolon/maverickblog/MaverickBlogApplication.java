0d0682fed78d32846ff530b65b89eebcf1a76767
Victor-Maverick <victormsonter@gmail.com>
2024-04-10 15:47:22 +0100

    first commit

---
+++ src/main/java/africa/semicolon/maverickblog/MaverickBlogApplication.java
@@ -0,0 +1,13 @@
+package africa.semicolon.maverickblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaverickBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaverickBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/africa/semicolon/maverickblog/MaverickBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package africa.semicolon.maverickblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaverickBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
