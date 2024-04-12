3d02cdc78cc2bcbc6467f8f1a2109d9bf8d32526
Victor-Maverick <victormsonter@gmail.com>
2024-04-12 09:33:20 +0100

    commit

--- src/main/java/africa/semicolon/maverickblog/controllers/UserController.java
+++ src/main/java/africa/semicolon/maverickblog/controllers/UserController.java
@@ -1,5 +1,6 @@
 package africa.semicolon.maverickblog.controllers;
 
+import africa.semicolon.maverickblog.dtos.requests.DeleteUserRequest;
 import africa.semicolon.maverickblog.dtos.requests.RegisterRequest;
 import africa.semicolon.maverickblog.dtos.responses.ApiResponse;
 import africa.semicolon.maverickblog.dtos.responses.RegisterResponse;
@@ -8,7 +9,9 @@ import africa.semicolon.maverickblog.services.UserServices;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.CREATED;
@@ -21,7 +24,7 @@ public class UserController {
     private UserServices userServices;
 
     @PostMapping("/register-user")
-    public ResponseEntity<?>register(RegisterRequest registerRequest) {
+    public ResponseEntity<?>register(@RequestBody RegisterRequest registerRequest) {
         try{
             RegisterResponse response = userServices.register(registerRequest);
             return new ResponseEntity<>(new ApiResponse(true, response), CREATED);
@@ -29,5 +32,10 @@ public class UserController {
         catch (MaverickBlogException e){
             return new ResponseEntity<>(new ApiResponse(false, e.getMessage()), HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @DeleteMapping("sign-off")
+    public ResponseEntity<?> deleteUser(@RequestBody DeleteUserRequest deleteUserRequest) {
+        try
     }
 }
--- src/test/java/africa/semicolon/maverickblog/controllers/UserControllerTest.java
+++ src/test/java/africa/semicolon/maverickblog/controllers/UserControllerTest.java
@@ -1,6 +1,7 @@
 package africa.semicolon.maverickblog.controllers;
 
 import africa.semicolon.maverickblog.data.repository.Users;
+import africa.semicolon.maverickblog.dtos.requests.DeleteUserRequest;
 import africa.semicolon.maverickblog.dtos.requests.RegisterRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,10 @@ public class UserControllerTest {
         registerRequest.setPhoneNumber("08148624687");
         userController.register(registerRequest);
         assertEquals(1, users.count());
-
+        DeleteUserRequest deleteUserRequest = new DeleteUserRequest();
+        deleteUserRequest.setUsername("username4");
+        deleteUserRequest.setPassword("password");
+        userController.deleteUser(deleteUserRequest);
+        assertEquals(1, users.count());
     }
 }
